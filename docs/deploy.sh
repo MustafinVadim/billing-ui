deb8ad8d7fd8df6a184a9c4e27bc8a70e185ed9c
Ivan Sosnin <vansosnin@gmail.com>
2016-11-17 22:51:25 +0500

    Причесал скрипт деплоя

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,27 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
 
 OUTPUT_DIR="dist"
 REPO_URL="git@github.com:skbkontur/billing-ui.git"
 COMMITTER_EMAIL="isosnin@skbkontur.ru"
 
+function highlighted_echo {
+    echo -e "\e[1m$1\e[21m";
+}
+
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 
-echo -e "\e[93mBuilding storybook...\e[97m"
+echo ""
+highlighted_echo "Building storybook to ./$OUTPUT_DIR..."
 npm run build-storybook -- -o $OUTPUT_DIR
+highlighted_echo "Building storybook done"
+echo ""
 
-echo -e "\e[93mInitializing git...\e[97m"
+highlighted_echo "Initializing git..."
 cd ./$OUTPUT_DIR
 git init
 git config user.name "GH Pages Bot"
 git config user.email $COMMITTER_EMAIL
 git add .
 git commit -m "Deploy Storybook to GitHub Pages"
+highlighted_echo "Initializing git done"
+echo ""
 
-echo -e "\e[93mDeploying storybook...\e[97m"
+highlighted_echo "Deploying storybook to $REPO_URL..."
 git push --force --quiet $REPO_URL master:gh-pages
+highlighted_echo "Deploying storybook done"
+echo ""
 
 cd ..
 rm -rf $OUTPUT_DIR
 
-echo -e "\e[1m\e[92mSuccessfully deployed!\e[97m"
+highlighted_echo "Documentation deployed successfully!"
+echo ""
