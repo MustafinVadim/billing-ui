10aa74a2fd4ddff84ef0d449417719b2dd75b971
Ivan Sosnin <isosnin@skbkontur.ru>
2016-12-07 19:05:03 +0500

    Правки EncodeHelpers

--- helpers/EncodeHelpers.js
+++ helpers/EncodeHelpers.js
@@ -1,20 +1,15 @@
-export const safeDecodeURI = (text) => {
-    try {
-        return text ? decodeURIComponent(text) : text;
-    } catch (e) {
-        return text
-    }
-};
-
-export const safeEncodeURI = (text) => {
+export const safeEncodeURI = text => {
     try {
         return text ? encodeURIComponent(text) : text;
     } catch (e) {
-        return text
+        return text;
     }
 };
 
-export default {
-    safeDecodeURI,
-    safeEncodeURI
+export const safeDecodeURI = text => {
+    try {
+        return text ? decodeURIComponent(text) : text;
+    } catch (e) {
+        return text;
+    }
 };
