fbe578717aa99f83daaa810136bc4bc393c3dff0
Ivan Sosnin <isosnin@skbkontur.ru>
2017-01-11 16:28:51 +0500

    Добавили возможность отправлять массив событий в аналитику

--- helpers/GoogleAnalytics.js
+++ helpers/GoogleAnalytics.js
@@ -1,6 +1,14 @@
 const GaEventHelper = window.GaEventHelper || null;
 
 class GoogleAnalytics {
+    static triggerEventsAsync(events) {
+        if (Array.isArray(events)) {
+            events.forEach(evt => {
+                GoogleAnalytics.triggerEventAsync(evt.category, evt.action, evt.label);
+            });
+        }
+    }
+
     static triggerEventAsync(category, action, label) {
         GaEventHelper && GaEventHelper.publishEventAsync(category, action, label);
     }
