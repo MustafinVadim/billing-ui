4f75f03dc8cbc22fff57bb2817c3814d5936204c
Ivan Sosnin <isosnin@skbkontur.ru>
2017-01-17 11:19:49 +0500

    Правка генерации сообщения для логгера

--- helpers/Logger/helpers.js
+++ helpers/Logger/helpers.js
@@ -1,6 +1,6 @@
 export const generateAjaxErrorMessage = ({url, requestMethod, data, errorMessage}) => [
     `REQUEST URL: ${url}`,
     `HTTP METHOD: ${requestMethod}`,
-    `REQUEST DATA: ${data}`,
+    `REQUEST DATA: ${JSON.stringify(data)}`,
     `ERROR MESSAGE: ${errorMessage}`
 ];
