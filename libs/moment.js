40a1d7d3aadb7c426e60eba1b2ad7c1ac3933cad
Ivan Sosnin <isosnin@skbkontur.ru>
2017-02-07 14:38:10 +0500

    Добавил новый хелпер для форматирования даты+времени

--- libs/moment.js
+++ libs/moment.js
@@ -2,6 +2,7 @@ import moment from "moment";
 moment.locale("ru");
 
 export const formatDate = (date, format = "L") => moment(date).format(format);
+export const formatDateWithTime = date => moment(date).format("DD.MM.YYYY HH:mm");
 export const convertISOString = (date) => convertString(date, moment.ISO_8601);
 export const convertString = (date, format = "DD.MM.YYYY") => moment(date, format, true);
 export const convertToISO = (date) => {
