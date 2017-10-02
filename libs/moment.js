5a16104b104036650f37a57fa93e4afba111c348
Ivan Sosnin <isosnin@skbkontur.ru>
2017-10-02 11:39:24 +0500

    Рефакторинг + тесты на адаптер moment

--- libs/moment.js
+++ libs/moment.js
@@ -3,13 +3,19 @@ import "moment/locale/ru";
 
 moment.locale("ru");
 
-export const formatDate = (date, format = "L") => moment(date).format(format);
-export const formatDateWithTime = date => moment(date).format("DD.MM.YYYY HH:mm");
+export const formatDate = (date, format = "L") => {
+    const momentDate = moment(date);
+    return momentDate.isValid() ? momentDate.format(format) : null;
+};
+export const formatDateWithTime = (date) => formatDate(date, "DD.MM.YYYY HH:mm");
+export const convertString = (date, format = "DD.MM.YYYY") => {
+    const momentDate = moment(date, format, true);
+    return momentDate.isValid() ? momentDate : null;
+};
 export const convertISOString = (date) => convertString(date, moment.ISO_8601);
-export const convertString = (date, format = "DD.MM.YYYY") => moment(date, format, true);
 export const convertToISO = (date) => {
     const momentDate = moment(date, moment.ISO_8601);
-    return momentDate.isValid() ? momentDate.toISOString() : convertString(date).toISOString();
+    return momentDate.isValid() ? momentDate.toISOString() : null;
 };
 
 export const inRange = (date, minDate, maxDate) => !!date && moment(date).isValid() && moment(date).isBetween(minDate || null, maxDate || null, "day", "[]");
--- test/libs/moment.test.js
+++ test/libs/moment.test.js
@@ -1,62 +1,111 @@
 import { expect } from "chai";
-import moment, { formatDate, inRange } from "../../libs/moment";
+import moment, { formatDate, inRange, formatDateWithTime, convertString, convertISOString, convertToISO } from "../../libs/moment";
 
-describe("moment formatDate helper", () => {
-    it("should format with rule L", () => {
-        const actual = formatDate("2013-02-01T00:00:00.000");
-        expect(actual).to.equal("01.02.2013");
+describe("moment adapter", () => {
+    describe("formatDate", () => {
+        it("should format with rule L", () => {
+            expect(formatDate("2013-02-01T00:00:00.000")).to.equal("01.02.2013");
+        });
+
+        it("should format with rule LT", () => {
+            expect(formatDate("2013-02-01T11:13:00.000", "LT")).to.equal("11:13");
+        });
+
+        it("should return null for null date", () => {
+            expect(formatDate(null)).to.be.null;
+        });
     });
 
-    it("should format with rule LT", () => {
-        const actual = formatDate("2013-02-01T11:13:00.000", "LT");
-        expect(actual).to.equal("11:13");
-    });
-});
+    describe("formatDateWithTime", () => {
+        it("should format date correctly", () => {
+            expect(formatDateWithTime("2013-02-01T11:13:00.000")).to.equal("01.02.2013 11:13");
+        });
 
-describe("moment inRange helper", () => {
-    const monday = moment([2017, 8, 28]);
-    const tuesday = moment([2017, 8, 29]);
-    const wednesday = moment([2017, 8, 30]);
-
-    it("should handle range as inclusive", () => {
-        expect(inRange(monday, monday, wednesday)).to.be.true;
-        expect(inRange(wednesday, monday, wednesday)).to.be.true;
+        it("should return null for invalud date", () => {
+            expect(formatDateWithTime(null)).to.be.null;
+        });
     });
 
-    it("should work with ISO string", () => {
-        const between = tuesday.toISOString();
-        const from = monday.toISOString();
-        const to = wednesday.toISOString();
+    describe("convertString", () => {
+        it("should convert date string to moment", () => {
+            expect(convertString("02.10.2017").toISOString()).to.deep.equal("2017-10-01T19:00:00.000Z");
+        });
 
-        expect(inRange(between, from, to)).to.be.true;
-        expect(inRange(from, between, to)).to.be.false;
+        it("should return null for invalid date", () => {
+            expect(convertString(null)).to.be.null;
+        });
     });
 
-    it("should work with Date", () => {
-        const between = tuesday.toDate();
-        const from = monday.toDate();
-        const to = wednesday.toDate();
+    describe("convertISOString", () => {
+        it("should convert date in ISO format to moment", () => {
+            expect(convertISOString("2013-02-01T11:13:00.000").toISOString()).to.equal("2013-02-01T06:13:00.000Z");
+        });
 
-        expect(inRange(between, from, to)).to.be.true;
-        expect(inRange(from, between, to)).to.be.false;
+        it("should return null for invalid date", () => {
+            expect(convertISOString(null)).to.be.null;
+        });
     });
 
-    it("should be false if first argument is null or undefined", () => {
-        expect(inRange(null, monday, wednesday)).to.be.false;
-        expect(inRange(undefined, monday, wednesday)).to.be.false;
+    describe("convertToISO", () => {
+        it("should convert string date to ISO format", () => {
+            expect(convertToISO("2013-02-01T11:13:00.000")).to.equal("2013-02-01T06:13:00.000Z");
+        });
+
+        it("should convert moment date to ISO format", () => {
+            const now = moment();
+            expect(convertToISO(now)).to.equal(now.toISOString());
+        });
+
+        it("should return null for invalid date", () => {
+            expect(convertToISO(null)).to.be.null;
+        });
     });
 
-    it("should handle null or undefined in minDate and maxDate parameters as open range", () => {
-        expect(inRange(monday, null, tuesday)).to.be.true;
-        expect(inRange(wednesday, null, tuesday)).to.be.false;
-        expect(inRange(wednesday, tuesday, undefined)).to.be.true;
-        expect(inRange(monday, tuesday, undefined)).to.be.false;
-    });
+    describe("inRange", () => {
+        const monday = moment([2017, 8, 28]);
+        const tuesday = moment([2017, 8, 29]);
+        const wednesday = moment([2017, 8, 30]);
 
-    it("should handle invalid date in minDate and maxDate parameters as open range", () => {
-        expect(inRange(monday, moment(null), tuesday)).to.be.true;
-        expect(inRange(wednesday, moment(null), tuesday)).to.be.false;
-        expect(inRange(wednesday, tuesday, moment(null))).to.be.true;
-        expect(inRange(monday, tuesday, moment(null))).to.be.false;
+        it("should handle range as inclusive", () => {
+            expect(inRange(monday, monday, wednesday)).to.be.true;
+            expect(inRange(wednesday, monday, wednesday)).to.be.true;
+        });
+
+        it("should work with ISO string", () => {
+            const between = tuesday.toISOString();
+            const from = monday.toISOString();
+            const to = wednesday.toISOString();
+
+            expect(inRange(between, from, to)).to.be.true;
+            expect(inRange(from, between, to)).to.be.false;
+        });
+
+        it("should work with Date", () => {
+            const between = tuesday.toDate();
+            const from = monday.toDate();
+            const to = wednesday.toDate();
+
+            expect(inRange(between, from, to)).to.be.true;
+            expect(inRange(from, between, to)).to.be.false;
+        });
+
+        it("should be false if first argument is null or undefined", () => {
+            expect(inRange(null, monday, wednesday)).to.be.false;
+            expect(inRange(undefined, monday, wednesday)).to.be.false;
+        });
+
+        it("should handle null or undefined in minDate and maxDate parameters as open range", () => {
+            expect(inRange(monday, null, tuesday)).to.be.true;
+            expect(inRange(wednesday, null, tuesday)).to.be.false;
+            expect(inRange(wednesday, tuesday, undefined)).to.be.true;
+            expect(inRange(monday, tuesday, undefined)).to.be.false;
+        });
+
+        it("should handle invalid date in minDate and maxDate parameters as open range", () => {
+            expect(inRange(monday, moment(null), tuesday)).to.be.true;
+            expect(inRange(wednesday, moment(null), tuesday)).to.be.false;
+            expect(inRange(wednesday, tuesday, moment(null))).to.be.true;
+            expect(inRange(monday, tuesday, moment(null))).to.be.false;
+        });
     });
 });
