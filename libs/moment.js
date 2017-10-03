74454df5e20f0547ba3f86833d9855d571723b66
Ivan Sosnin <isosnin@skbkontur.ru>
2017-10-03 16:01:32 +0500

    Fix convertToISO bug

--- libs/moment.js
+++ libs/moment.js
@@ -15,7 +15,16 @@ export const convertString = (date, format = "DD.MM.YYYY") => {
 export const convertISOString = (date) => convertString(date, moment.ISO_8601);
 export const convertToISO = (date) => {
     const momentDate = moment(date, moment.ISO_8601);
-    return momentDate.isValid() ? momentDate.toISOString() : null;
+
+    if (momentDate.isValid()) {
+        return momentDate.toISOString();
+    }
+
+    if (convertString(date)) {
+        return convertString(date).toISOString();
+    }
+
+    return null;
 };
 
 export const inRange = (date, minDate, maxDate) => !!date && moment(date).isValid() && moment(date).isBetween(minDate || null, maxDate || null, "day", "[]");
