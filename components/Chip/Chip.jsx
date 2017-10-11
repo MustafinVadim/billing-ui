44284875c0b08ff3d62f9212b7189cf65a80e4b3
Ivan Sosnin <isosnin@skbkontur.ru>
2017-10-11 16:53:57 +0500

    Компонент Chip

---
+++ components/Chip/Chip.jsx
@@ -0,0 +1,42 @@
+import { PureComponent } from "react";
+import PropTypes from "prop-types";
+import omit from "lodash/omit";
+
+import Icon, { IconTypes } from "../Icon";
+
+import styles from "./Chip.scss";
+import cx from "classnames";
+
+class Chip extends PureComponent {
+    render() {
+        const { className, children, iconClassName, onRemove } = this.props;
+        const fieldsToOmit = ["className", "children", "iconClassName", "onRemove", "ref", "key"];
+        const spanProps = omit(this.props, fieldsToOmit);
+
+        return (
+            <span
+                className={cx(styles.content, className)}
+                {...spanProps}
+            >
+                {children}
+                <Icon
+                    onClick={onRemove}
+                    className={cx(styles.icon, iconClassName)}
+                    type={IconTypes.Delete}
+                />
+            </span>
+        );
+    }
+}
+
+Chip.propTypes = {
+    className: PropTypes.string,
+    iconClassName: PropTypes.string,
+
+    children: PropTypes.string,
+
+    onRemove: PropTypes.func,
+    onMouseDown: PropTypes.func
+};
+
+export default Chip;
