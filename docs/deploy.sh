3c4d406ca4302929d3891bc535968426e03e28b4
Ivan Sosnin <isosnin@skbkontur.ru>
2018-01-11 16:35:46 +0500

    Revert "Try fix storybook deploy"
    This reverts commit c941d96bc95509d7a1b705e3ce5ae52c42547c7a.

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-OUTPUT_DIR="./dist"
+OUTPUT_DIR="dist"
 REPO_URL="git@github.com:skbkontur/billing-ui.git"
 COMMITTER_EMAIL="isosnin@skbkontur.ru"
 
@@ -8,19 +8,17 @@ function highlighted_echo {
     echo -e "\e[1m$1\e[21m";
 }
 
-highlighted_echo "Check dir"
-ls -la
 rm -rf $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 
 echo ""
-highlighted_echo "Building storybook to $OUTPUT_DIR..."
+highlighted_echo "Building storybook to ./$OUTPUT_DIR..."
 npm run build-storybook -- -o $OUTPUT_DIR
 highlighted_echo "Building storybook done"
 echo ""
 
 highlighted_echo "Initializing git..."
-cd $OUTPUT_DIR
+cd ./$OUTPUT_DIR
 git init
 git config user.name "GH Pages Bot"
 git config user.email $COMMITTER_EMAIL
